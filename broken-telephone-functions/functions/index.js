76c67a3ee8b74180dc80afe124fe9684223f0c1d
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-21 15:34:11 -0400

    created image upload for user profile

--- broken-telephone-functions/functions/index.js
+++ broken-telephone-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')()
 const FBAuth = require('./util/FBAuth')
 
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signUp, logIn } = require('./handlers/users')
+const { signUp, logIn, uploadImage } = require('./handlers/users')
 
 // Screams routes
 app.get('/screams', getAllScreams)
@@ -14,5 +14,6 @@ app.post('/scream', FBAuth, postOneScream)
 // Users route
 app.post('/signup', signUp)
 app.post('/login', logIn)
+app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app)
--- broken-telephone-functions/functions/util/FBAuth.js
+++ broken-telephone-functions/functions/util/FBAuth.js
@@ -1,5 +1,4 @@
-const { admin } = require('./admin')
-const { db } = require('../util/admin')
+const { admin, db } = require('./admin')
 
 module.exports = (req, res, next) => {
   let idToken
