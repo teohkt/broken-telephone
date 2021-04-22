42a665b07c7fb4938366c326aa6b765d9dfae2fa
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-21 21:08:16 -0400

    created like and unlike for screams

--- broken-telephone-functions/functions/index.js
+++ broken-telephone-functions/functions/index.js
@@ -4,19 +4,25 @@ const app = require('express')()
 
 const FBAuth = require('./util/FBAuth')
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+} = require('./handlers/screams')
 const { signUp, logIn, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 // Screams routes
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
 app.get('/scream/:screamId', getScream)
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 
 //TODO: Delete Scream
-//TODO: Like Scream
-//TODO: Unlike Scream
-//TODO: Comment on scream
-app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
 // Users route
 app.post('/signup', signUp)
