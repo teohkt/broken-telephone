67332a8fd001d0b3ffb11bf6d4942101176d1071
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-22 11:45:46 -0400

    updated error messages

--- broken-telephone-functions/functions/handlers/screams.js
+++ broken-telephone-functions/functions/handlers/screams.js
@@ -67,7 +67,7 @@ exports.getScream = (req, res) => {
 
 // Comment on scream
 exports.commentOnScream = (req, res) => {
-  if (req.body.body.trim() === '') return res.status(400).json({ error: 'Must not be empty' })
+  if (req.body.body.trim() === '') return res.status(400).json({ comment: 'Must not be empty' })
 
   const newComment = {
     body: req.body.body,
--- broken-telephone-functions/functions/handlers/users.js
+++ broken-telephone-functions/functions/handlers/users.js
@@ -54,7 +54,7 @@ exports.signUp = (req, res) => {
       if (err.code === 'auth/email-already-in-use') {
         return res.status(400).json({ email: 'Email is already in use' })
       } else {
-        return res.status(500).json({ error: err.code })
+        return res.status(500).json({ general: 'Something went wrong, please try again' })
       }
     })
 }
@@ -80,9 +80,7 @@ exports.logIn = (req, res) => {
     })
     .catch((err) => {
       console.error(err)
-      if (err.code === 'auth/wrong-password') {
-        return res.status(403).json({ general: 'Wrong credentials, please try again ' })
-      } else return res.status(500).json({ error: err.code })
+      return res.status(403).json({ general: 'Wrong credentials, please try again ' })
     })
 }
 
