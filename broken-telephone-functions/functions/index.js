c98f9c3122c2d3d50f095ed5416eb9991c4bfc34
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-22 11:39:12 -0400

    added functions to create notifications with firestore triggers, added routes to get different user profiles and mark notifications as read

--- broken-telephone-functions/functions/index.js
+++ broken-telephone-functions/functions/index.js
@@ -4,6 +4,8 @@ const app = require('express')()
 
 const FBAuth = require('./util/FBAuth')
 
+const { db } = require('./util/admin')
+
 const {
   getAllScreams,
   postOneScream,
@@ -13,7 +15,15 @@ const {
   unlikeScream,
   deleteScream,
 } = require('./handlers/screams')
-const { signUp, logIn, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
+const {
+  signUp,
+  logIn,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead,
+} = require('./handlers/users')
 
 // Screams routes
 app.get('/screams', getAllScreams)
@@ -24,13 +34,68 @@ app.get('/scream/:screamId/like', FBAuth, likeScream)
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 app.delete('/scream/:screamId/', FBAuth, deleteScream)
 
-//TODO: Delete Scream
-
 // Users route
 app.post('/signup', signUp)
 app.post('/login', logIn)
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
+app.get('/user/:handle', getUserDetails)
+app.post('/notifications', markNotificationsRead)
 
 exports.api = functions.https.onRequest(app)
+
+// Database trigger, not endpoint. No need to return json values (error, data)
+exports.createNotificationOnLike = functions.firestore.document('/likes/{id}').onCreate((snapshot) => {
+  return db
+    .doc(`/screams/${snapshot.data().screamId}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'like',
+          read: false,
+          screamId: doc.id,
+        })
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      return
+    })
+})
+
+exports.deleteNotificationOnUnlike = functions.firestore.document('/likes/{id}').onDelete((snapshot) => {
+  return db
+    .doc(`/notifications/${snapshot.id}`)
+    .delete()
+    .catch((err) => {
+      console.error(err)
+      return
+    })
+})
+
+exports.createNotificationOnComment = functions.firestore.document('/comments/{id}').onCreate((snapshot) => {
+  return db
+    .doc(`/screams/${snapshot.data().screamId}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'comment',
+          read: false,
+          screamId: doc.id,
+        })
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      return
+    })
+})
