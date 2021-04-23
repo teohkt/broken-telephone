1ec8c2b8ed96f4e92ad21e546b452d263a648cb6
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-22 20:56:21 -0400

    updated navbar layout

--- broken-telephone-client/src/components/Navbar.js
+++ broken-telephone-client/src/components/Navbar.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom' //Cant cherry pick, will throw error
 //Material UI
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 const Navbar = () => {
   return (
     <AppBar>
-      <Toolbar className='nav-container'>
-        <Button color='inherit' component={Link} to={'/login'}>
-          Login
-        </Button>
-        <Button color='inherit' component={Link} to={'/home'}>
-          Home
-        </Button>
-        <Button color='inherit' component={Link} to={'/signup'}>
-          Signup
-        </Button>
-      </Toolbar>
+      <Container>
+        <Toolbar className='nav-container'>
+          <Button color='inherit' component={Link} to={'/login'}>
+            Login
+          </Button>
+          <Button color='inherit' component={Link} to={'/home'}>
+            Home
+          </Button>
+          <Button color='inherit' component={Link} to={'/signup'}>
+            Signup
+          </Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
