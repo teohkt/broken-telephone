3a94acd46178cb8517a0766adaf83fa92f1c01e5
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-24 00:56:12 -0400

    added component that can redirect routes if un-expired token is found

--- broken-telephone-client/src/App.js
+++ broken-telephone-client/src/App.js
@@ -2,15 +2,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import jwtDecode from 'jwt-decode'
+
 // Components
 import Navbar from './components/Navbar'
 import themeObject from './util/theme'
+import AuthRoute from './util/AuthRoute'
+
 // Pages
 import Home from './Screens/Home'
 import Login from './Screens/Login'
 import Signup from './Screens/Signup'
 
 const theme = createMuiTheme(themeObject)
+
+let authenticated
+const token = localStorage.FBIdToken
+if (token) {
+  const decodedToken = jwtDecode(token)
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login'
+    authenticated = false
+  } else {
+    authenticated = true
+    console.log(authenticated)
+  }
+}
 
 function App() {
   return (
@@ -20,8 +37,8 @@ function App() {
         <div className='container'>
           <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
+            <AuthRoute path='/login' component={Login} authenticated={authenticated} />
+            <AuthRoute path='/signup' component={Signup} authenticated={authenticated} />
           </Switch>
         </div>
       </Router>
--- broken-telephone-client/src/components/Navbar.js
+++ broken-telephone-client/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
           <Button color='inherit' component={Link} to={'/login'}>
             Login
           </Button>
-          <Button color='inherit' component={Link} to={'/home'}>
+          <Button color='inherit' component={Link} to={'/'}>
             Home
           </Button>
           <Button color='inherit' component={Link} to={'/signup'}>
